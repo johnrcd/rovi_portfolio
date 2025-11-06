aca8b9dbba2e5193aebed753e533d6942b20e1d1
johnrcd <80877409+johnrcd@users.noreply.github.com>
2025-11-06 04:21:08 -0600

    refactor: remove unused references, plugins, and assets

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,8 @@
 import { IdAttributePlugin, InputPathToUrlTransformPlugin, HtmlBasePlugin } from "@11ty/eleventy";
-import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import markdownIt from "markdown-it";
 import markdownItAnchor from "markdown-it-anchor";
-
-import pluginFilters from "./_config/filters.js";
 
 import YAML from "yaml";
 
@@ -55,50 +51,6 @@ export default async function(eleventyConfig) {
 	eleventyConfig.addPlugin(HtmlBasePlugin);
 	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
 
-	eleventyConfig.addPlugin(feedPlugin, {
-		type: "atom", // or "rss", "json"
-		outputPath: "/feed.xml",
-		collection: {
-			name: "post",
-			limit: 1000,
-		},
-		metadata: {
-			language: "en",
-			title: "Eleventy PagesCMS Project Template",
-			subtitle: "Project Template for a static site built with Eleventy and Paper CMS.",
-			base: "https://example.com/",
-			author: {
-				name: "Rovi Decena",
-				email: "johnrcd.ar@gmail.com",
-				url: "https://rovidecena.com/"
-			}
-		}
-	});
-
-	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
-	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-		// Output formats for each image.
-		formats: ["avif", "webp", "auto"],
-
-		// widths: ["auto"],
-
-		failOnError: false,
-		htmlOptions: {
-			imgAttributes: {
-				// e.g. <img loading decoding> assigned on the HTML tag will override these values.
-				loading: "lazy",
-				decoding: "async",
-			}
-		},
-
-		sharpOptions: {
-			animated: true,
-		},
-	});
-
-	// Filters
-	eleventyConfig.addPlugin(pluginFilters);
-
 	eleventyConfig.addPlugin(IdAttributePlugin, {
 		// by default we use Eleventy’s built-in `slugify` filter:
 		// slugify: eleventyConfig.getFilter("slugify"),
@@ -126,19 +78,9 @@ export default async function(eleventyConfig) {
 
 	eleventyConfig.setLibrary("md", markdownLib);
 	eleventyConfig.addDataExtension("yaml", (contents) => YAML.parse(contents));
-
-	// Features to make your build faster (when you need them)
-
-	// If your passthrough copy gets heavy and cumbersome, add this line
-	// to emulate the file copy on the dev server. Learn more:
-	// https://www.11ty.dev/docs/copy/#emulate-passthrough-copy-during-serve
-
-	// eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 };
 
 export const config = {
-	// Control which files Eleventy will process
-	// e.g.: *.md, *.njk, *.html, *.liquid
 	templateFormats: [
 		"md",
 		"njk",
@@ -147,30 +89,15 @@ export const config = {
 		"11ty.js",
 	],
 
-	// Pre-process *.md files with: (default: `liquid`)
 	markdownTemplateEngine: "njk",
-
-	// Pre-process *.html files with: (default: `liquid`)
 	htmlTemplateEngine: "njk",
 
-	// These are all optional:
 	dir: {
-		input: "content",          // default: "."
-		includes: "../_includes",  // default: "_includes" (`input` relative)
-		data: "../_data",          // default: "_data" (`input` relative)
+		input: "content",
+		includes: "../_includes",
+		data: "../_data",
 		output: "_site"
 	},
-
-	// -----------------------------------------------------------------
-	// Optional items:
-	// -----------------------------------------------------------------
-
-	// If your site deploys to a subdirectory, change `pathPrefix`.
-	// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
-
-	// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
-	// it will transform any absolute URLs in your HTML to include this
-	// folder name and does **not** affect where things go in the output folder.
 
 	// pathPrefix: "/",
 };
