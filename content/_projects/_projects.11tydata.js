96e4d2ea2cfe0dd886c259f2d0ff86a49958696f
johnrcd <80877409+johnrcd@users.noreply.github.com>
2025-11-06 04:17:57 -0600

    feat: set up yaml to store site data

--- content/_projects/_projects.11tydata.js
+++
@@ -1,4 +0,0 @@
-export default {
-	tags: ["data_projects"],
-	eleventyExcludeFromCollections: true,
-};
--- content/_sections/_sections.11tydata.js
+++
@@ -1,4 +0,0 @@
-export default {
-	tags: ["data_sections"],
-	eleventyExcludeFromCollections: true,
-};
