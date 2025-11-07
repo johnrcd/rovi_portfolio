2569eecfd09419cc09181a8d306e0ce53374714d
johnrcd <80877409+johnrcd@users.noreply.github.com>
2025-11-07 03:51:20 -0600

    feat: update home page

--- content/content.11tydata.js
+++ content/content.11tydata.js
@@ -1,11 +1,3 @@
-// 11tydata.js files act as a base
-
-// this file only affects templates within the same directory () because it shares the
-// same name as the directory it resides in
-
-// [content]/[content].11tydata.js
-
-
 export default {
 	layout: "layouts/home.njk",
 };
